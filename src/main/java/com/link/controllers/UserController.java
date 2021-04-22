b828f262f40c6338d738483b539d5d33920c7620
christianrkent <christianrkent@gmail.com>
2021-04-22 11:09:00 -0500

    settting up DB, NOT WORKING :(

--- src/main/java/com/link/controllers/UserController.java
+++ src/main/java/com/link/controllers/UserController.java
@@ -1,210 +1,210 @@
-package com.link.controllers;
-
-import com.link.model.User;
-import com.link.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.List;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-
-
-
-@RestController
-@CrossOrigin
-@RequestMapping("/users")
-public class UserController {
-    UserController userController;
-    private UserService userService;
-    private PasswordEncoder passwordEncoder;
-    final static Logger loggy = Logger.getLogger(UserController.class);
-    static {
-        loggy.setLevel(Level.ALL);
-        //loggy.setLevel(Level.ERROR);
-    }
-
-    /**
-     * Api endpoint for the main landing page of application that allows user to login.
-     * @param session HTTP session
-     * @param user User object of the current logged in user.
-     * @return User object
-     */
-    //TODO: need to figure out how to use auth token for log-in
-    @PostMapping("/login")
-    public User logIn(HttpSession session,@RequestBody User user){
-        /*
-        since the password is now encrypted the new user must be retrieved by
-        its username instead of by a dummy user object, as it was pre pw hashing.
-        */
-        User newUser= userService.getUserByUserName(user.getUserName());
-
-        //check if the PW input on client side matches the encrypted PW in our DB
-        if(passwordEncoder.matches(user.getPassword(),newUser.getPassword())){
-            //same login logic as before password encryption
-            session.setAttribute("loggedInUser", newUser);
-
-            User currentUser = (User) session.getAttribute("loggedInUser");
-            return newUser;
-
-        }else return new User();        // should fix this that return empty user
-    }
-
-    /**
-     * Api endpoint that inserts User object into the application depending on whether they exists or not.
-     * @param user User object.
-     * @return Custom response message (string).
-     */
-    @PostMapping(value = "/insertNewUser")
-    public void insertNewUser(@RequestBody User user){
-        User alreadyExists = getUserByUsername(user);
-        if(alreadyExists == null) {
-            userService.createUser(user);
-            loggy.info("The successful creation of a user with username: "+user.getUserName()+".");
-        }
-        else {
-            loggy.info("The failed creation of a user with username: "+user.getUserName()+".");
-        }
-    }
-
-//    /**
-//     * Api endpoint that receives User object to use username to retreive User object from service layer.
-//     * @param user User object from HTTP request.
-//     * @return User object based on the username.
-//     */
-//    @PostMapping(value = "/getByUsername")
-//    public User getUserByUsername(@RequestBody User user) {
-//        loggy.info("An attempt to retrieve a user with username: "+user.getUserName()+".");
-//        return userService.getUserByUserName(user.getUserName());
+//package com.link.controllers;
+//
+//import com.link.model.User;
+//import com.link.service.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpSession;
+//import java.util.List;
+//import org.apache.log4j.Level;
+//import org.apache.log4j.Logger;
+//
+//
+//
+//@RestController
+//@CrossOrigin
+//@RequestMapping("/users")
+//public class UserController {
+////    UserController userController;
+////    private UserService userService;
+////    private PasswordEncoder passwordEncoder;
+////    final static Logger loggy = Logger.getLogger(UserController.class);
+////    static {
+////        loggy.setLevel(Level.ALL);
+////        //loggy.setLevel(Level.ERROR);
+////    }
+////
+////    /**
+////     * Api endpoint for the main landing page of application that allows user to login.
+////     * @param session HTTP session
+////     * @param user User object of the current logged in user.
+////     * @return User object
+////     */
+////    //TODO: need to figure out how to use auth token for log-in
+////    @PostMapping("/login")
+/////*    public User logIn(HttpSession session,@RequestBody User user){
+////        *//*
+////        since the password is now encrypted the new user must be retrieved by
+////        its username instead of by a dummy user object, as it was pre pw hashing.
+////        *//*
+////        User newUser= userService.getUserByUserName(user.getUserName());
+////
+////        //check if the PW input on client side matches the encrypted PW in our DB
+////        if(passwordEncoder.matches(user.getPassword(),newUser.getPassword())){
+////            //same login logic as before password encryption
+////            session.setAttribute("loggedInUser", newUser);
+////
+////            User currentUser = (User) session.getAttribute("loggedInUser");
+////            return newUser;
+////
+////        }else return new User();        // should fix this that return empty user
+////    }
+////
+////    *//**
+////     * Api endpoint that inserts User object into the application depending on whether they exists or not.
+////     * @param user User object.
+////     * @return Custom response message (string).
+////     *//*
+////    @PostMapping(value = "/insertNewUser")
+////    public void insertNewUser(@RequestBody User user){
+////        User alreadyExists = getUserByUsername(user);
+////        if(alreadyExists == null) {
+////            userService.createUser(user);
+////            loggy.info("The successful creation of a user with username: "+user.getUserName()+".");
+////        }
+////        else {
+////            loggy.info("The failed creation of a user with username: "+user.getUserName()+".");
+////        }
+////    }*/
+////
+//////    /**
+//////     * Api endpoint that receives User object to use username to retreive User object from service layer.
+//////     * @param user User object from HTTP request.
+//////     * @return User object based on the username.
+//////     */
+/////*    @PostMapping(value = "/getByUsername")
+////    public User getUserByUsername(@RequestBody User user) {
+////        loggy.info("An attempt to retrieve a user with username: "+user.getUserName()+".");
+////        return userService.getUserByUserName(user.getUserName());
+////    }*/
+////
+////    /**
+////     * Api endpoint that returns an Array list of User objects from the service layer.
+////     * @return Array list of all registered User objects in the HTTP response body.
+////     */
+////    @GetMapping(value="/getAllUsers")
+////    public List<User> getAllUsers(){
+////        return userService.getAllUsers();
+////    }
+////
+////
+////    /**
+////     * Api endpoint that returns a User object from the service layer.
+////     * @param userId User identifier (Int).
+////     * @return User object in the HTTP response.
+////     */
+////    @GetMapping(value = "/getUserById/{userId}")
+////    public User getUserById(@PathVariable("userId") int userId){
+////        return userService.getUserByID(userId);
+////    }
+////
+////    /**
+////     * Api endpoint that updates the User in the application. Receives updated User object
+////     * and passes the information to the service layer. Returns message when the user is updated
+////     * in the application via response body.
+////     * @param session HTTP Session of current logged in user.
+////     * @param user User object.
+////     * @return Custom response message (string)
+////     */
+////    //TODO: might change the session into auth token
+////    @PutMapping(value = "/updateUser")
+////    public void updateUser(HttpSession session,@RequestBody User user){
+////        if(session.getAttribute("loggedInUser")!=null){
+////
+////            //TODO: need to update with auth tokens
+////            User current=((User)session.getAttribute("loggedInUser"));
+////
+////            //TODO: need to update the update user vs update password
+////            if(!current.getPassword().equals(user.getPassword())){
+////                loggy.info("The successful update(with password) of a user with username: "+user.getUserName()+".");
+////                userService.updateUser(user);
+////            }
+////            else {
+////                loggy.info("The successful update of a user with username: "+user.getUserName()+".");
+////                userService.updateUser(user);
+////            }
+////
+////            User updatedVersion=userService.getUserByID(current.getUserID());
+////            session.setAttribute("loggedInUser",updatedVersion);
+////        }
+////        else{
+////            loggy.info("The failed update of a user with username: "+user.getUserName()+".");
+////        }
+////
+////    }
+////
+////    /**
+////     * Api endpoint that will remove a User from the application. Returns message when the user
+////     * is deleted through the HTTP response body.
+////     * @param user User object.
+////     * @return Custom response message (string).
+////     */
+////    @DeleteMapping(value = "/deleteUser")
+////    public void deleteUser(@RequestBody User user){
+////        userService.deleteUser(user);
+////        loggy.info("The deletion of a user with username: "+user.getUserName()+".");
+////    }
+////
+////    /**
+////     * Api endpoint that sends an email to User's email with a randomly generated password.
+////     * @param userName Username of User current user (string).
+////     * @return Custom response message (string).
+////     */
+////    //TODO: need to ask team for this kind of implementation, might need to add different service for this
+////
+////
+////    /**
+////     * Api endpoint that logs out the user from the application. Redirects to landing page.
+////     * @param myReq HTTP servlet request
+////     */
+////    //TODO: might change the session into auth token
+////    @GetMapping(value = "logout")
+////    public void logout(HttpServletRequest myReq){
+////
+////        //TODO: need to refactor fro auth token
+////        HttpSession userSession = myReq.getSession();
+////        loggy.info("The successful logout of the session:"+userSession);
+////        userSession.invalidate();
+////    }
+////
+////    /**
+////     * Api endpoint that retrieves the logged in user from the HTTP session.
+////     * @param session HTTP session
+////     * @return User object
+////     */
+////    //TODO: might change the session into auth token
+////    @GetMapping(value = "/getLoggedInUser")
+////    public User getLoggedInUser(HttpSession session){
+////        //try to get the most updated version of the user
+////        if(session.getAttribute("loggedInUser")!=null){
+////            int id = ((User)session.getAttribute("loggedInUser")).getUserID();
+////            User updatedVersion=userService.getUserByID(id);
+////            session.setAttribute("loggedInUser",updatedVersion);
+////
+////            loggy.info("The successful retrieval of the loggedInUser");
+////            return (User) session.getAttribute("loggedInUser");
+////        }
+////        else{
+////            loggy.info("The failed retrieval of the loggedInUser");
+////            return null;
+////        }
+////    }
+////
+////    //upload profile image (might be in update user)
+////
+//
+//
+//
+//    public UserController() {
 //    }
-
-    /**
-     * Api endpoint that returns an Array list of User objects from the service layer.
-     * @return Array list of all registered User objects in the HTTP response body.
-     */
-    @GetMapping(value="/getAllUsers")
-    public List<User> getAllUsers(){
-        return userService.getAllUsers();
-    }
-
-
-    /**
-     * Api endpoint that returns a User object from the service layer.
-     * @param userId User identifier (Int).
-     * @return User object in the HTTP response.
-     */
-    @GetMapping(value = "/getUserById/{userId}")
-    public User getUserById(@PathVariable("userId") int userId){
-        return userService.getUserByID(userId);
-    }
-
-    /**
-     * Api endpoint that updates the User in the application. Receives updated User object
-     * and passes the information to the service layer. Returns message when the user is updated
-     * in the application via response body.
-     * @param session HTTP Session of current logged in user.
-     * @param user User object.
-     * @return Custom response message (string)
-     */
-    //TODO: might change the session into auth token
-    @PutMapping(value = "/updateUser")
-    public void updateUser(HttpSession session,@RequestBody User user){
-        if(session.getAttribute("loggedInUser")!=null){
-
-            //TODO: need to update with auth tokens
-            User current=((User)session.getAttribute("loggedInUser"));
-
-            //TODO: need to update the update user vs update password
-            if(!current.getPassword().equals(user.getPassword())){
-                loggy.info("The successful update(with password) of a user with username: "+user.getUserName()+".");
-                userService.updateUser(user);
-            }
-            else {
-                loggy.info("The successful update of a user with username: "+user.getUserName()+".");
-                userService.updateUser(user);
-            }
-
-            User updatedVersion=userService.getUserByID(current.getUserID());
-            session.setAttribute("loggedInUser",updatedVersion);
-        }
-        else{
-            loggy.info("The failed update of a user with username: "+user.getUserName()+".");
-        }
-
-    }
-
-    /**
-     * Api endpoint that will remove a User from the application. Returns message when the user
-     * is deleted through the HTTP response body.
-     * @param user User object.
-     * @return Custom response message (string).
-     */
-    @DeleteMapping(value = "/deleteUser")
-    public void deleteUser(@RequestBody User user){
-        userService.deleteUser(user);
-        loggy.info("The deletion of a user with username: "+user.getUserName()+".");
-    }
-
-    /**
-     * Api endpoint that sends an email to User's email with a randomly generated password.
-     * @param userName Username of User current user (string).
-     * @return Custom response message (string).
-     */
-    //TODO: need to ask team for this kind of implementation, might need to add different service for this
-
-
-    /**
-     * Api endpoint that logs out the user from the application. Redirects to landing page.
-     * @param myReq HTTP servlet request
-     */
-    //TODO: might change the session into auth token
-    @GetMapping(value = "logout")
-    public void logout(HttpServletRequest myReq){
-
-        //TODO: need to refactor fro auth token
-        HttpSession userSession = myReq.getSession();
-        loggy.info("The successful logout of the session:"+userSession);
-        userSession.invalidate();
-    }
-
-    /**
-     * Api endpoint that retrieves the logged in user from the HTTP session.
-     * @param session HTTP session
-     * @return User object
-     */
-    //TODO: might change the session into auth token
-    @GetMapping(value = "/getLoggedInUser")
-    public User getLoggedInUser(HttpSession session){
-        //try to get the most updated version of the user
-        if(session.getAttribute("loggedInUser")!=null){
-            int id = ((User)session.getAttribute("loggedInUser")).getUserID();
-            User updatedVersion=userService.getUserByID(id);
-            session.setAttribute("loggedInUser",updatedVersion);
-
-            loggy.info("The successful retrieval of the loggedInUser");
-            return (User) session.getAttribute("loggedInUser");
-        }
-        else{
-            loggy.info("The failed retrieval of the loggedInUser");
-            return null;
-        }
-    }
-
-    //upload profile image (might be in update user)
-
-
-
-
-    public UserController() {
-    }
-
-    @Autowired
-    public UserController(UserController userController, UserService userService, PasswordEncoder passwordEncoder) {
-        this.userController = userController;
-        this.userService = userService;
-        this.passwordEncoder = passwordEncoder;
-    }
-}
+//
+//    @Autowired
+//    public UserController(UserController userController, UserService userService, PasswordEncoder passwordEncoder) {
+//        this.userController = userController;
+//        this.userService = userService;
+//        this.passwordEncoder = passwordEncoder;
+//    }
+//}
--- src/main/java/com/link/dao/UserDaoImpl.java
+++ src/main/java/com/link/dao/UserDaoImpl.java
@@ -1,3 +1,4 @@
+/*
 package com.link.dao;
 
 import com.link.model.User;
@@ -15,32 +16,38 @@ public class UserDaoImpl implements UserDao{
 
     private SessionFactory sesFact;
 
-    /** Authors: Chris B, Christian K, Dang L, Nick H
+    */
+/** Authors: Chris B, Christian K, Dang L, Nick H
      * Creates new user in DB, pass in all user info except ID
      *
      * @param user All info except ID, that's generated in the DB.
      *
-     */
+     *//*
+
     @Override
     public void createUser(User user){
         sesFact.getCurrentSession().save(user);
     }
 
-    /** Authors: Chris B, Christian K, Dang L, Nick H
+    */
+/** Authors: Chris B, Christian K, Dang L, Nick H
      * Returns a specific user chosen by ID
      * @param userID target user's ID
      * @return the user with the matching ID
-     */
+     *//*
+
     @Override
     public User getUserByID(int userID){
         return sesFact.getCurrentSession().get(User.class, userID);
     }
 
-    /** Authors: Chris B, Christian K, Dang L, Nick H
+    */
+/** Authors: Chris B, Christian K, Dang L, Nick H
      * Returns a specific user chosen by username
      * @param userName target user's username
      * @return the user with the matching username
-     */
+     *//*
+
     @Override
     public User getUserByUserName(String userName) {
         List<User> user= sesFact.getCurrentSession().createQuery("from User where username= '"+ userName +"'").list();
@@ -53,30 +60,36 @@ public class UserDaoImpl implements UserDao{
         return output;
     }
 
-    /** Authors: Chris B, Christian K, Dang L, Nick H
+    */
+/** Authors: Chris B, Christian K, Dang L, Nick H
      * Getting all User as a list from database
      * And return all User
      * @param
      * @return All user
-     */
+     *//*
+
     @Override
     public List<User> getAllUsers() {
         return sesFact.getCurrentSession().createQuery("from UserAccount", User.class).list();
     }
 
-    /** Authors: Chris B, Christian K, Dang L, Nick H
+    */
+/** Authors: Chris B, Christian K, Dang L, Nick H
      * Replaces old user data in the DB with new user data given as input. Id should never change!
      * @param user The new data
-     */
+     *//*
+
     @Override
     public void updateUser(User user){
         sesFact.getCurrentSession().update(user);
     }
 
-    /** Authors: Chris B, Christian K, Dang L, Nick H
+    */
+/** Authors: Chris B, Christian K, Dang L, Nick H
      * Deleting user
      * @param user with current data
-     */
+     *//*
+
     @Override
     public void deleteUser(User user){
         sesFact.getCurrentSession().delete(user);
@@ -98,3 +111,4 @@ public class UserDaoImpl implements UserDao{
     public UserDaoImpl() {
     }
 }
+*/
