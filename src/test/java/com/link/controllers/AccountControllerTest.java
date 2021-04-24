72ce512ab1c715d1228d938646a1ea54bf206002
houstonlws <houstonhvrd@gmail.com>
2021-04-24 17:52:00 -0400

    Slight change to tests

--- src/test/java/com/link/controllers/AccountControllerTest.java
+++ src/test/java/com/link/controllers/AccountControllerTest.java
@@ -6,20 +6,25 @@ import com.link.util.PasswordAuthentication;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class AccountControllerTest {
 
+    @Autowired
     private UserServiceImpl userService;
+
+    @Autowired
     private JWTServiceImpl jwtService;
+
+    @Autowired
     private PasswordAuthentication authorizer;
 
     @BeforeEach
     void setUp() {
-        userService = new UserServiceImpl();
-        jwtService = new JWTServiceImpl();
-        authorizer = new PasswordAuthentication();
     }
 
     @AfterEach
