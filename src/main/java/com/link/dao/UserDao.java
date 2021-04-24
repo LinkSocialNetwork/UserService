f13c3c1837727b87794e440021511189091ec9c5
michaeldegennaro <michaeldegennaro58@yahoo.com>
2021-04-24 13:06:49 -0400

    pull

--- src/main/java/com/link/dao/UserDao.java
+++ src/main/java/com/link/dao/UserDao.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Repository
 public interface UserDao extends JpaRepository<User, Integer> {
     public User findByUserName(String userName);
+    public User findById(int id);
+
 }
